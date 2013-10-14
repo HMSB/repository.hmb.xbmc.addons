e77cb9bb3807b094fdd453a4b1946324421a6345
HMSB <husseinbahaidarah@HusseinMacbookAir.local>
2013-10-14 19:10:44 +0300

    0.4.0

---
+++ plugin.video.okanime/default.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import urllib
+import urllib2
+import socket
+import sys
+import re
+import os
+import datetime
+import xbmcplugin
+import xbmcgui
+import xbmcaddon
+import json
+
+socket.setdefaulttimeout(30)
+pluginhandle = int(sys.argv[1])
+addon = xbmcaddon.Addon()
+addonID = addon.getAddonInfo('id')
+thumbsDir = xbmc.translatePath('special://home/addons/'+addonID+'/resources/thumbs')
+forceViewMode = addon.getSetting("forceViewMode") == "true"
+useThumbAsFanart = addon.getSetting("useThumbAsFanart") == "true"
+autoPlay = int(addon.getSetting("autoPlay"))
+viewModeNewsShows = str(addon.getSetting("viewModeNewsShows"))
+viewModeVideos = str(addon.getSetting("viewModeVideos"))
+prefRes = addon.getSetting("prefRes")
+prefRes = [1080, 720, 520, 480, 360, 240][int(prefRes)]
+itemsPerPage = addon.getSetting("itemsPerPage")
+itemsPerPage = ["25", "50", "75", "100"][int(itemsPerPage)]
+iconPathShows = os.path.join(thumbsDir, "shows.jpeg")
+iconPath_AnimeMovie = os.path.join(thumbsDir, "anime.jpeg")
+iconPathMost_watched = os.path.join(thumbsDir, "top5.jpeg")
+iconPathMore = os.path.join(thumbsDir, "more.jpeg")
+iconPathAtoZ = os.path.join(thumbsDir, "AtoZ.png")
+urlShows = "http://okanime.com/category/anime/page/1/" 
+urlNewitems = ""
+urlSearch = ""
+
+def index(): 
+    addDir("Latest Items آخر الإضافات", urlShows, 'listShows', iconPathShows)
+    addDir("A-Z", urlSearch, 'showMessage', iconPathAtoZ)
+    addDir("Anime Movies",urlNewitems, 'showMessage', iconPath_AnimeMovie)
+    addDir("Top 5", "", 'showMessage', iconPathMost_watched)
+    
+    if forceViewMode:
+        xbmc.executebuiltin('Container.SetViewMode('+viewModeNewsShows+')')
+    xbmcplugin.endOfDirectory(pluginhandle)
+
+
+def listShows(url):
+    htmlfile = urllib.urlopen(url)
+    htmltext = htmlfile.read()
+    htmltext = re.sub(r'<img src="http://okanime.com/images/widget-stars.png(.|\n)*$', '', htmltext, re.S)
+    htmltext = re.sub("&#(\d+)(;|(?=\s))", '', htmltext)
+    regex1 = '''<a href="http://okanime.com/'''+'''[^,/]*/"'''+''' title="(.*?)">\n'''
+    regex2 = '''<img src="(.*?)" alt="'''
+   # regex3 = '''class="post-([0-9]*?) post type-post status-'''
+    regex3 = '''<span class="page-title2"><a href="http://okanime.com/(.*?)/" title="'''
+    regex4 = '''/page/(.*?)/'''
+    regex5 = '''<a href="http://okanime.com/episodes/(.*?)/" rel="tag">'''
+    pattern1 = re.compile(regex1)
+    pattern2 = re.compile(regex2)
+    pattern3 = re.compile(regex3)
+    pattern4 = re.compile(regex4)
+    pattern5 = re.compile(regex5)
+    show_name = re.findall(pattern1,htmltext)
+    img_path = re.findall(pattern2,htmltext)
+    showID = re.findall(pattern3,htmltext)
+    pageNum = re.findall(pattern4,url)
+    numOfEpisodes = re.findall(pattern5,htmltext)
+    i = 0
+    while i< len(show_name):
+        addDir(show_name[i], showID[i], 'listEpsodes', img_path[i])
+        i+=1
+    pageCount = int(pageNum[0]) + 1
+    url = re.sub("/page/.*","/page/"+str(pageCount)+"/", url)
+    addDir("More", url, 'listShows', iconPathMore)
+    if forceViewMode:
+        xbmc.executebuiltin('Container.SetViewMode('+viewModeNewsShows+')')
+    xbmcplugin.endOfDirectory(pluginhandle)
+
+def listEpsodes(showID):
+    #numOfEpisodes = 26
+   # urlCh = "http://okanime.com/?post_type=episode&p=" + str(int(showID)+14)
+    urlCh = "http://okanime.com/" + showID
+    htmlfile = urllib.urlopen(urlCh)
+    htmltext = htmlfile.read()
+    regex1 = '''p=(.*?)"'''
+    pattern1 = re.compile(regex1)
+    episodeID = re.findall(pattern1,htmltext)  
+    if  len(episodeID)<1:
+        showMessage("No Episodes Yet", "لم تتم اضافة اي حلقة بعد","")
+        numOfEpisodes = []
+        i = 100
+    else:
+        urlCh = "http://okanime.com/?post_type=episode&p=" + episodeID[0]
+        htmlfile = urllib.urlopen(urlCh)
+        htmltext = htmlfile.read()
+        regex1 = '''/?cat=(.*?)"'''
+        pattern1 = re.compile(regex1)
+        showID2 = re.findall(pattern1,htmltext)
+        urleps = "http://okanime.com/episode/?cat=" + showID2[0]
+       # showMessage(showID2[0], showID, urlCh)
+        htmlfile = urllib.urlopen(urleps)
+        htmltext = htmlfile.read()
+        regex1 = '(.*?) الحلقة'
+        regex2 = '''p=(.*?)"'''
+        pattern1 = re.compile(regex1)
+        pattern2 = re.compile(regex2)
+        numOfEpisodes = re.findall(pattern1,htmltext) 
+        episodeID = re.findall(pattern2,htmltext) 
+        i = 0  
+    while i< len(numOfEpisodes):
+        addLink("الحلقة رقم " + str(i+1), "http://okanime.com/?post_type=episode&p=" + episodeID[i], 'playVideo', '', 'Plot', 000, 'date', str(i))
+        i+=1
+    if forceViewMode:
+        xbmc.executebuiltin('Container.SetViewMode('+viewModeNewsShows+')')
+    xbmcplugin.endOfDirectory(pluginhandle)
+
+
+def playVideo(url):
+   # htmlfile = urllib.urlopen(url)
+   # htmltext = htmlfile.read()
+   # regex1 = '''<a href=(.*?) target=FRAME1 class="stream-link">google</a>'''
+   # pattern1 = re.compile(regex1)
+    #video_path = re.findall(pattern1,htmltext)
+    #video_path = "https://r5---sn-cg07luee.c.docs.google.com/videoplayback?requiressl=yes&shardbypass=yes&cmbypass=yes&id=3f43cf8b0a8d0b2a&itag=18&source=webdrive&app=docs&ip=86.51.182.21&ipbits=0&expire=1381638254&sparams=requiressl,shardbypass,cmbypass,id,itag,source,ip,ipbits,expire&signature=6B9CDD2A7AEC4B8DC467FB0BBF6C2D09AB6828BF.86C57875B0438EE9555171037A2C7683F18394B3&key=ck2&ir=1&ms=tsu&mt=1381634589&mv=m&cpn=hbs574nTpPTFmwpW"
+    #video_path = "https://r8---sn-cg07luee.c.docs.google.com/videoplayback?requiressl=yes&shardbypass=yes&cmbypass=yes&id=aaccf970a874fed4&itag=35&source=webdrive&app=docs&ip=86.51.182.21&ipbits=0&expire=1381684165&sparams=requiressl,shardbypass,cmbypass,id,itag,source,ip,ipbits,expire&signature=1D6F5EE04F757025C9D61BDAA4FF0BBE66B75190.42D57AA26AF651CC3AD50DCD2C74EBC9D043C6AB&key=ck2&ir=1&ms=tsu&mt=1381680491&mv=m&type=video/x-flv&quality=large"
+    htmlfile = urllib.urlopen(url)
+    htmltext = htmlfile.read()
+    regex1 = '''<a href=(.*?) target=FRAME1 class="stream-link">google</a>'''
+    pattern1 = re.compile(regex1)
+    ch_name = re.findall(pattern1,htmltext)
+    regex1 = '''http://okanime.com/player/google/.(.*?)&'''
+    pattern1 = re.compile(regex1)
+    videoList = re.findall(pattern1,ch_name[0])
+    url = "http://docs.google.com/file/d/" + videoList[0] + "/preview"
+    htmlfile = urllib.urlopen(url)
+    htmltext = htmlfile.read()
+    regex3 = '''url=(.*?)&type='''
+    pattern3 = re.compile(regex3)
+    urlVideo = re.findall(pattern3,htmltext)
+    video_path = urllib.unquote_plus(urlVideo[0])
+    #video_path = "https://r8---sn-cg07luee.c.docs.google.com/videoplayback?requiressl=yes&shardbypass=yes&cmbypass=yes&id=aaccf970a874fed4&itag=35&source=webdrive&app=docs&ip=86.51.182.21&ipbits=0&expire=1381685119&sparams=requiressl,shardbypass,cmbypass,id,itag,source,ip,ipbits,expire&signature=245774BC5AC2C3F56BD44EDA398A23A521251C3A.8209C9759524F29ABDE2D216736AFCB3D5B85D4C&key=ck2&ir=1&ms=tsu&mt=1381681455&mv=m"
+    listitem = xbmcgui.ListItem(path=video_path)
+    #listitem.setInfo(type="Video", infoLabels={ "plot": desc})
+    xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+    #xbmc.executebuiltin('XBMC.Notification(%s, 5000)'%(video_path)) 
+
+    
+def addLink(name, url, mode, iconimage, desc, length="", date="", nr=""):
+    u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
+    ok = True
+    liz = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+    liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": desc, "Aired": date, "Episode": nr})
+    if length:
+        liz.addStreamInfo('video', {'duration': int(length)})
+    liz.setProperty('IsPlayable', 'true')
+    if useThumbAsFanart:
+        liz.setProperty("fanart_image", iconimage)
+    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz)
+    return ok
+   
+    
+def showMessage(msg1,msg2, msg3):
+        __addon__       = xbmcaddon.Addon()
+        __addonname__   = __addon__.getAddonInfo('name')
+        #xbmc.executebuiltin('Notification(%s, 50000)'%(msg))
+        xbmcgui.Dialog().ok(__addonname__, msg1, msg2, msg3)
+        
+def parameters_string_to_dict(parameters):
+    paramDict = {}
+    if parameters:
+        paramPairs = parameters[1:].split("&")
+        for paramsPair in paramPairs:
+            paramSplits = paramsPair.split('=')
+            if (len(paramSplits)) == 2:
+                paramDict[paramSplits[0]] = paramSplits[1]
+    return paramDict
+
+def addDir(name, url, mode, iconimage, type="", desc=""):
+    u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&type="+str(type)
+    ok = True
+    liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+    liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": desc})
+    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
+    return ok
+
+params = parameters_string_to_dict(sys.argv[2])
+mode = urllib.unquote_plus(params.get('mode', ''))
+url = urllib.unquote_plus(params.get('url', ''))
+type = urllib.unquote_plus(params.get('type', ''))
+
+
+if mode == 'playVideo':
+    playVideo(url)    
+elif mode == 'listEpsodes':
+    listEpsodes(url)
+elif mode == 'listShows':
+    listShows(url)
+elif mode == 'listChannels':
+    listChannels()
+elif mode == 'showMessage':
+    showMessage('Coming Soon','','')
+else:
+    index()
+
